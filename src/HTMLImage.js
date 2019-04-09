c894a973b1b96eeb7091755b4f28ecd10301ff93
Tạ Minh Tiên <taminhtien1993@gmail.com>
2019-04-09 09:10:28 +0700

    Add image preview to HTMLImage

--- src/HTMLImage.js
+++ src/HTMLImage.js
@@ -1,137 +1,205 @@
-import React, { PureComponent } from 'react';
-import { Image, View, Text } from 'react-native';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import { Image, View, Text, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import ImageView from "react-native-image-view";
 
 export default class HTMLImage extends PureComponent {
-    constructor (props) {
-        super(props);
-        this.state = {
-            width: props.imagesInitialDimensions.width,
-            height: props.imagesInitialDimensions.height,
-            indeterminate: (!props.style || !props.style.width || !props.style.height)
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: props.imagesInitialDimensions.width,
+      height: props.imagesInitialDimensions.height,
+      indeterminate: !props.style || !props.style.width || !props.style.height,
+      isImageViewVisible: false
+    };
+  }
 
-    static propTypes = {
-        source: PropTypes.object.isRequired,
-        alt: PropTypes.string,
-        height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-        width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-        style: Image.propTypes.style,
-        imagesMaxWidth: PropTypes.number,
-        imagesInitialDimensions: PropTypes.shape({
-            width: PropTypes.number,
-            height: PropTypes.number
-        })
-    }
+  static propTypes = {
+    source: PropTypes.object.isRequired,
+    alt: PropTypes.string,
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    style: Image.propTypes.style,
+    imagesMaxWidth: PropTypes.number,
+    imagesInitialDimensions: PropTypes.shape({
+      width: PropTypes.number,
+      height: PropTypes.number
+    })
+  };
 
-    static defaultProps = {
-        imagesInitialDimensions: {
-            width: 100,
-            height: 100
+  static defaultProps = {
+    imagesInitialDimensions: {
+      width: 100,
+      height: 100
+    }
+  };
+
+  componentDidMount() {
+    this.getImageSize();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.getImageSize(nextProps);
+  }
+
+  getDimensionsFromStyle(style, height, width) {
+    let styleWidth;
+    let styleHeight;
+
+    if (height) {
+      styleHeight = height;
+    }
+    if (width) {
+      styleWidth = width;
+    }
+    if (Array.isArray(style)) {
+      style.forEach(styles => {
+        if (!width && styles["width"]) {
+          styleWidth = styles["width"];
         }
-    }
-
-    componentDidMount () {
-        this.getImageSize();
-    }
-
-    componentWillReceiveProps (nextProps) {
-        this.getImageSize(nextProps);
-    }
-
-    getDimensionsFromStyle (style, height, width) {
-        let styleWidth;
-        let styleHeight;
-
-        if (height) {
-            styleHeight = height;
+        if (!height && styles["height"]) {
+          styleHeight = styles["height"];
         }
-        if (width) {
-            styleWidth = width;
+      });
+    } else {
+      if (!width && style["width"]) {
+        styleWidth = style["width"];
+      }
+      if (!height && style["height"]) {
+        styleHeight = style["height"];
+      }
+    }
+
+    return { styleWidth, styleHeight };
+  }
+
+  getImageSize(props = this.props) {
+    const { source, imagesMaxWidth, style, height, width } = props;
+    const { styleWidth, styleHeight } = this.getDimensionsFromStyle(
+      style,
+      height,
+      width
+    );
+
+    if (styleWidth && styleHeight) {
+      return this.setState({
+        width:
+          typeof styleWidth === "string" && styleWidth.search("%") !== -1
+            ? styleWidth
+            : parseInt(styleWidth, 10),
+        height:
+          typeof styleHeight === "string" && styleHeight.search("%") !== -1
+            ? styleHeight
+            : parseInt(styleHeight, 10),
+        indeterminate: false
+      });
+    }
+    // Fetch image dimensions only if they aren't supplied or if with or height is missing
+    Image.getSize(
+      source.uri,
+      (originalWidth, originalHeight) => {
+        if (!imagesMaxWidth) {
+          return this.setState({
+            width: originalWidth,
+            height: originalHeight
+          });
         }
-        if (Array.isArray(style)) {
-            style.forEach((styles) => {
-                if (!width && styles['width']) {
-                    styleWidth = styles['width'];
-                }
-                if (!height && styles['height']) {
-                    styleHeight = styles['height'];
-                }
-            });
-        } else {
-            if (!width && style['width']) {
-                styleWidth = style['width'];
-            }
-            if (!height && style['height']) {
-                styleHeight = style['height'];
-            }
-        }
+        const optimalWidth =
+          imagesMaxWidth <= originalWidth ? imagesMaxWidth : originalWidth;
+        const optimalHeight = (optimalWidth * originalHeight) / originalWidth;
+        this.setState({
+          width: optimalWidth,
+          height: optimalHeight,
+          indeterminate: false,
+          error: false
+        });
+      },
+      () => {
+        this.setState({ error: true });
+      }
+    );
+  }
 
-        return { styleWidth, styleHeight };
+  validImage(source, style, props = {}) {
+    const images = [
+      {
+        source: source,
+        width: this.state.width,
+        height: this.state.height
+      }
+    ];
+
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={() => this.setState({ isImageViewVisible: true })}
+        >
+          <Image
+            source={source}
+            style={[
+              style,
+              {
+                width: this.state.width,
+                height: this.state.height,
+                resizeMode: "cover"
+              }
+            ]}
+            {...props}
+          />
+          <ImageView
+            images={images}
+            imageIndex={0}
+            isVisible={this.state.isImageViewVisible}
+            onClose={() => this.setState({ isImageViewVisible: false })}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  get errorImage() {
+    return (
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          borderWidth: 1,
+          borderColor: "lightgray",
+          overflow: "hidden",
+          justifyContent: "center"
+        }}
+      >
+        {this.props.alt ? (
+          <Text style={{ textAlign: "center", fontStyle: "italic" }}>
+            {this.props.alt}
+          </Text>
+        ) : (
+          false
+        )}
+      </View>
+    );
+  }
+
+  get placeholderImage() {
+    return (
+      <View
+        style={{
+          width: this.props.imagesInitialDimensions.width,
+          height: this.props.imagesInitialDimensions.height
+        }}
+      />
+    );
+  }
+
+  render() {
+    const { source, style, passProps } = this.props;
+
+    if (this.state.error) {
+      return this.errorImage;
     }
-
-    getImageSize (props = this.props) {
-        const { source, imagesMaxWidth, style, height, width } = props;
-        const { styleWidth, styleHeight } = this.getDimensionsFromStyle(style, height, width);
-
-        if (styleWidth && styleHeight) {
-            return this.setState({
-                width: typeof styleWidth === 'string' && styleWidth.search('%') !== -1 ? styleWidth : parseInt(styleWidth, 10),
-                height: typeof styleHeight === 'string' && styleHeight.search('%') !== -1 ? styleHeight : parseInt(styleHeight, 10),
-                indeterminate: false
-            });
-        }
-        // Fetch image dimensions only if they aren't supplied or if with or height is missing
-        Image.getSize(
-            source.uri,
-            (originalWidth, originalHeight) => {
-                if (!imagesMaxWidth) {
-                    return this.setState({ width: originalWidth, height: originalHeight });
-                }
-                const optimalWidth = imagesMaxWidth <= originalWidth ? imagesMaxWidth : originalWidth;
-                const optimalHeight = (optimalWidth * originalHeight) / originalWidth;
-                this.setState({ width: optimalWidth, height: optimalHeight, indeterminate: false, error: false });
-            },
-            () => {
-                this.setState({ error: true });
-            }
-        );
+    if (this.state.indeterminate) {
+      return this.placeholderImage;
     }
-
-    validImage (source, style, props = {}) {
-        return (
-            <Image
-              source={source}
-              style={[style, { width: this.state.width, height: this.state.height, resizeMode: 'cover' }]}
-              {...props}
-            />
-        );
-    }
-
-    get errorImage () {
-        return (
-            <View style={{ width: 50, height: 50, borderWidth: 1, borderColor: 'lightgray', overflow: 'hidden', justifyContent: 'center' }}>
-                { this.props.alt ? <Text style={{ textAlign: 'center', fontStyle: 'italic' }}>{ this.props.alt }</Text> : false }
-            </View>
-        );
-    }
-
-    get placeholderImage() {
-        return (
-            <View style={{ width: this.props.imagesInitialDimensions.width, height: this.props.imagesInitialDimensions.height }} />
-        );
-    }
-
-    render () {
-        const { source, style, passProps } = this.props;
-
-        if (this.state.error) {
-            return this.errorImage;
-        }
-        if (this.state.indeterminate) {
-            return this.placeholderImage;
-        }
-        return this.validImage(source, style, passProps);
-    }
+    return this.validImage(source, style, passProps);
+  }
 }
